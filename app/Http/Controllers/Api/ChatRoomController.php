715bbe470131f7444ec826aeb3619b240df9424c
omiya0555 <otoharadak@gmail.com>
2024-10-17 07:57:53 +0900

    Create a store method in chatRoomController.
    In this method,Create a Chatroom and some ChatRoomUsers record.

--- app/Http/Controllers/Api/ChatRoomController.php
+++ app/Http/Controllers/Api/ChatRoomController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\ChatRoom;
+use App\Models\ChatRoomUser;
+use Illuminate\Support\Facades\DB;
+
 class ChatRoomController extends Controller
 {
     public function index()
@@ -12,9 +16,48 @@ class ChatRoomController extends Controller
         // ログインユーザーが所属するチャットルームの一覧を返す
     }
 
+    // 新しいチャットルームを作成する
     public function store(Request $request)
     {
-        // 新しいチャットルームを作成する
+        // バリデーション
+        $validated = $request->validate([
+            'room_name'       => 'sometimes|required|max:30',
+            'chat_user_ids'   => 'required|array|min:2', // 2人以上
+            'chat_user_ids.*' => 'exists:users,id',
+            'group_chat_flag' => 'required|boolean',
+        ]);
+
+        try {
+            DB::beginTransaction();
+
+            // チャットルームの作成
+            $chatRoom = ChatRoom::create([
+                'room_name' => $validated['group_chat_flag'] ? $validated['room_name'] : null,
+            ]);
+
+            // チャットルームにユーザーを追加
+            foreach ($validated['chat_user_ids'] as $chatUserId) {
+                ChatRoomUser::create([
+                    'chat_room_id' => $chatRoom->id,
+                    'user_id'      => $chatUserId,
+                ]);
+            }
+
+            DB::commit();
+
+            return response()->json([
+                'message' => 'ChatRoom created successfully!',
+                'chat_room' => $chatRoom
+            ], 201);
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            return response()->json([
+                'message' => 'ChatRoom creation failed!',
+                'error'   => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function show(string $id)
