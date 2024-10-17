7811d243973b79d358057157bb0a000076030dc0
omiya0555 <otoharadak@gmail.com>
2024-10-18 06:54:33 +0900

    Create a index method.
    There are only 5 users in this app.So I use User::all().

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,29 +4,45 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
+    // 全ユーザーの取得
     public function index()
     {
-        // 全ユーザーの取得
+        try {
+            // ユーザーを取得
+            // ユーザー数は５人で固定
+            $users = User::all();
+            return response()->json([
+                'users' => $users
+            ], 200);
+    
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Failed to fetch users',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function store(Request $request)
     {
-        // 特定のユーザーの取得
+        // ユーザー登録処理
         // 優先度　[　低　]
     }
 
     public function show(string $id)
     {
-        // ユーザー更新処理
-        // 優先度　[　中　]
+        // 特定のユーザーの取得
+        // 優先度　[　低　]
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        // ユーザー更新処理
+        // 優先度　[　低　]
     }
 
     public function destroy(string $id)
