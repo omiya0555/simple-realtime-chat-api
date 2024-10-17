b296f1e281319538b81f66e9d98f840eba1621be
omiya0555 <otoharadak@gmail.com>
2024-10-18 07:10:56 +0900

    Created a index method.
    Got messages and senders info by with('sender') relationship.

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -4,12 +4,36 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Message;
+use App\Models\ChatRoom;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
-    public function index()
+    // 特定のチャットルームのメッセージを取得する
+    public function index($chatRoomId)
     {
-        // 特定のチャットルームのメッセージを取得する
+        try{
+            // chatroom存在確認
+            $chatRoom = ChatRoom::findOrFail($chatRoomId);
+
+            // chatroom内のメッセージ取得、送信者情報付与
+            $messages = Message::where('chat_room_id', $chatRoom->id)
+                ->with('sender')    // リレーションにより送信ユーザーも取得
+                ->orderBy('created_at','asc')
+                ->get();
+            return response()->json([
+                'chat_room'     => $chatRoom,
+                'messages'      => $messages
+            ], 200);
+
+        }catch(\Exceptoin $e){
+            return response()->json([
+                'message' => 'Failed to fetch messages',
+                'error'   => $e->getMessage()
+            ], 500);
+        }
+        
     }
 
     public function store(Request $request)
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -20,6 +20,10 @@ class Message extends Model
     }
 
     // メッセージは1人の送信者に属する
+    
+    // messageの取得の際、with('sender')を追加する事で子のリレーションを使用
+    // =>sender情報を取得すると同時にユーザー情報もわかるので、
+    //   送信者のユーザー情報まで取得する事ができる。
     public function sender()
     {
         return $this->belongsTo(User::class, 'sender_id');
