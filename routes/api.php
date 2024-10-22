62aa8ca039c064c7150106c50b10c9b176fc035f
omiya0555 <otoharadak@gmail.com>
2024-10-22 19:51:22 +0900

    Add some config of API authenticate.
    Added HasApiTokens in UserModel.Added Kernel.php,user.index route.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\ChatRoomController;
 use App\Http\Controllers\Api\MessageController;
 use Illuminate\Support\Facades\Route;
@@ -12,10 +13,11 @@ Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanct
 // 認証が必要なルート群
 Route::middleware('auth:sanctum')->group(function () {
 
+    Route::get('users', [UserController::class, 'index']);
+
     // チャットルーム関連のルート
     Route::get('chat-rooms', [ChatRoomController::class, 'index']);  // ユーザーが参加しているチャットルーム一覧
     Route::post('chat-rooms', [ChatRoomController::class, 'store']); // 新しいチャットルームを作成
-    Route::get('chat-rooms/{id}', [ChatRoomController::class, 'show']);  // 特定のチャットルームの情報を取得
 
     // メッセージ関連のルート
     Route::get('chat-rooms/{chatRoomId}/messages', [MessageController::class, 'index']);  // 特定のチャットルームのメッセージ一覧
