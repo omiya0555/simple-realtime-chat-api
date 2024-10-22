1766952a344776900fa46dad139f144c02882f52
omiya0555 <otoharadak@gmail.com>
2024-10-22 20:13:08 +0900

    Create a Broadcasting configure file.
    Set the Pusher as default driver of laravel.

---
+++ config/broadcasting.php
@@ -0,0 +1,15 @@
+// Laravelのブロードキャスト設定ファイル
+// Pusherをデフォルトドライバとして設定
+
+'connections' => [
+    'pusher' => [
+        'driver' => 'pusher',
+        'key' => env('PUSHER_APP_KEY'),
+        'secret' => env('PUSHER_APP_SECRET'),
+        'app_id' => env('PUSHER_APP_ID'),
+        'options' => [
+            'cluster' => env('PUSHER_APP_CLUSTER'),
+            'useTLS' => true,
+        ],
+    ],
+],
