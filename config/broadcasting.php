bade4f54629dbc17439daaf71cdcd0ad9d7fe7f8
omiya0555 <otoharadak@gmail.com>
2024-10-22 20:37:25 +0900

    Create TestEvent.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -1,15 +1,35 @@
-// Laravelのブロードキャスト設定ファイル
-// Pusherをデフォルトドライバとして設定
+<?php
 
-'connections' => [
-    'pusher' => [
-        'driver' => 'pusher',
-        'key' => env('PUSHER_APP_KEY'),
-        'secret' => env('PUSHER_APP_SECRET'),
-        'app_id' => env('PUSHER_APP_ID'),
-        'options' => [
-            'cluster' => env('PUSHER_APP_CLUSTER'),
-            'useTLS' => true,
+return [
+
+    'default' => env('BROADCAST_DRIVER', 'null'),
+
+    'connections' => [
+
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
+            ],
         ],
+
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'default',
+        ],
+
+        'log' => [
+            'driver' => 'log',
+        ],
+
+        'null' => [
+            'driver' => 'null',
+        ],
+
     ],
-],
+
+];
